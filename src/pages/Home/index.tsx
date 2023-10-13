d1155cb3e043348c2c65ca4063f0cb862b9646a2
Vinicius Crispim <vinicrispim02@hotmail.com>
2023-10-13 14:14:47 -0300

    Seção projetos finalizado

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,8 +9,8 @@ export default function Home() {
         <>
             <Banner />
             <AboutMe />
-            <Certifications/>
             <Projects/>
+            <Certifications/>
         </>
     )
 }
